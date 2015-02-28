a7c551bb52f5eb1804af7baaee23d18d7d587261
Mike Thompson <mike@remind.me>
2015-02-28 10:19:59 -0500

    updated settings to include reminders app and updated models with first draft of fields

--- reminders/models.py
+++ reminders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	email = models.CharField(max_length=100)
+	password = models.CharField(max_length=100)
+	
+
+class Reminder(models.Model):
+	user = models.ForeignKey(User)
+	active = models.BooleanField(default=True)
+	title = models.CharField(max_length=100)
+	reminder_time = models.DateTimeField('reminder time')
+	snooze_duration = models.DateTimeField('snooze')
+	repeat_time = models.DateTimeField('repeat')
