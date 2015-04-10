14be8b86583c93c60b13ed609f34aeb6a5e9a428
Mike Thompson <mike@remind.me>
2015-04-10 07:31:35 -0400

    url file

---
+++ reminders/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from reminders import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+	url(r'^register/$', views.register, name='register'),
+	url(r'^login/$', views.user_login, name='login'),
+)
+
