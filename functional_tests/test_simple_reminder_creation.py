fe19d2d76ea1fc0816f1f18d4e1dbaa21fe06f04
Jim Triphahn <jimmtriphahn@gmail.com>
2015-06-18 19:17:16 -0700

    First cut of FT. Fails

--- functional_tests/test_simple_reminder_creation.py
+++ functional_tests/test_simple_reminder_creation.py
@@ -2,7 +2,7 @@ from .base import FunctionalTest
 
 from unittest import skip
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
 
 class NewVisitorTest(FunctionalTest):
 
@@ -12,12 +12,20 @@ class NewVisitorTest(FunctionalTest):
 		self.assertIn(u'Your Reminders', self.browser.title.decode())
 
 		# He then sees a button which says "Create a new reminder!"
-		#self.browser.find_element_by_id()
+		reminder_btn_text = self.browser.find_element_by_id('id_new_reminder_btn').text
+		self.assertIn("Create a new reminder!", reminder_btn_text)
 
 		# Billy clicks this button and a form window pops up
+		self.browser.find_element_by_id('id_new_reminder_btn').click()
+		self.assertTrue(expected_conditions.alert_is_present())
 
 		# In the form window, Billy sees fields for reminder title, reminder time, 
 		# reminder snooze, and reminder repeat options
+		modal = self.browser.switch_to.alert.text
+		self.assertIn('Reminder Title', modal)
+		self.assertIn('Reminder Time', modal)
+		self.assertIn('Reminder Snooze', modal)
+		self.assertIn('Reminder Repeat', modal)
 
 		#Finish the test
 		self.fail("Finish me")
