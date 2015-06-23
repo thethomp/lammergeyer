e7519513bdf970e058497bf5c5caf5484e963fb6
Jim Triphahn <jimmtriphahn@gmail.com>
2015-06-22 20:45:43 -0700

    Second cut of FT. Design decision: form as collapsible instead of modal.

--- functional_tests/test_simple_reminder_creation.py
+++ functional_tests/test_simple_reminder_creation.py
@@ -3,10 +3,12 @@ from .base import FunctionalTest
 from unittest import skip
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoAlertPresentException
 
 class NewVisitorTest(FunctionalTest):
 
-	def test_new_user_lands_on_home_page_and_sees_correct_title(self):
+	def test_new_user_lands_on_home_page_and_clicks_to_see_reminder_form(self):
 		# Billy lands on the home page and sees the web service title.
 		self.browser.get(self.server_url)
 		self.assertIn(u'Your Reminders', self.browser.title.decode())
@@ -15,17 +17,28 @@ class NewVisitorTest(FunctionalTest):
 		reminder_btn_text = self.browser.find_element_by_id('id_new_reminder_btn').text
 		self.assertIn("Create a new reminder!", reminder_btn_text)
 
-		# Billy clicks this button and a form window pops up
-		self.browser.find_element_by_id('id_new_reminder_btn').click()
-		self.assertTrue(expected_conditions.alert_is_present())
+		# Billy notices no form or content without clicking the button
+		panel = self.browser.find_element_by_id('id_new_reminder_panel')
+		self.assertTrue(panel is not None)
+		self.assertFalse(panel.is_displayed())
 
-		# In the form window, Billy sees fields for reminder title, reminder time, 
+		# Billy clicks this button and a panel is expanded showing the reminder format
+		self.browser.find_element_by_id('id_new_reminder_btn').click()
+		panel = self.browser.find_element_by_id('id_new_reminder_panel')
+		self.assertTrue(panel is not None)
+		self.assertTrue(panel.is_displayed())
+
+		# In the expanded content, Billy sees input fields for reminder title, reminder time, 
 		# reminder snooze, and reminder repeat options
-		modal = self.browser.switch_to.alert.text
-		self.assertIn('Reminder Title', modal)
-		self.assertIn('Reminder Time', modal)
-		self.assertIn('Reminder Snooze', modal)
-		self.assertIn('Reminder Repeat', modal)
+		form = self.browser.find_element_by_id('id_new_reminder_form')
+		placeholders = [input.get_attribute('placeholder') for input in form.find_elements_by_tag_name('input')]
+		expected_placeholders = ['Enter a reminder', 'Enter alarm', 'Enter snooze', 'Enter repeat']
+		self.assertEqual(placeholders, expected_placeholders)
+
+		# Billy is not ready to add a reminder so he collapses the form
+		reminder_btn_text = self.browser.find_element_by_id('id_new_reminder_btn')
+		reminder_btn_text.click()
+		self.assertEqual(reminder_btn_text.get_attribute('aria-expanded'), 'false')
 
 		#Finish the test
 		self.fail("Finish me")
