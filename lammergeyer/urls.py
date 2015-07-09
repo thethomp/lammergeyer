96844b424b0b1e0666332227365f9bd73c7e22f7
Jim Triphahn <jimmtriphahn@gmail.com>
2015-07-08 23:12:30 -0700

    refactored urls.py to make project and app more self-contained

--- lammergeyer/urls.py
+++ lammergeyer/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
-from reminders import views
+from reminders import views as reminders_views
+from reminders import urls as reminders_urls
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-	#url(r'^reminders/', include('reminders.urls')),
-	url(r'^$', views.home_page, name='home'),
-	url(r'^reminders/(\d+)/$', views.view_reminders, name='view_reminders'),
-	url(r'^reminders/(\d+)/add_reminder$', views.add_reminder, name='add_reminder'),
-	url(r'^reminders/new$', views.new_reminder_list, name='new_reminder_list'),
+    #url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', reminders_views.home_page, name='home'),
+	url(r'^reminders/', include(reminders_urls)),
+	#url(r'^$', views.home_page, name='home'),
+	#url(r'^reminders/(\d+)/$', views.view_reminders, name='view_reminders'),
+	#url(r'^reminders/(\d+)/add_reminder$', views.add_reminder, name='add_reminder'),
+	#url(r'^reminders/new$', views.new_reminder_list, name='new_reminder_list'),
 ]
--- reminders/urls.py
+++ reminders/urls.py
@@ -2,14 +2,17 @@ from django.conf.urls import patterns, url
 
 from reminders import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+urlpatterns = [
+    #url(r'^$', views.index, name='index'),
 	url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', views.user_login, name='login'),
 	url(r'^logged-in/$', views.user_logged_in, name='logged-in'),
-	url(r'^home/$', views.home, name='home'),
+	#url(r'^home/$', views.home, name='home'),
 	url(r'^account/$', views.account, name='account'),
 	url(r'^about/$', views.about, name='about'),
-	url(r'^logout/$', 'django.contrib.auth.views.logout', name="logout")
-)
+	url(r'^logout/$', 'django.contrib.auth.views.logout', name="logout"),
+	url(r'^new$', views.new_reminder_list, name='new_reminder_list'),
+	url(r'^(\d+)/$', views.view_reminders, name='view_reminders'),
+	url(r'^(\d+)/add_reminder$', views.add_reminder, name='add_reminder'),
+]
 
