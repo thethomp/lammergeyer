d04e8f1a0097ed74a9f90589640af9f0d93c56f5
Jim Triphahn <jimmtriphahn@gmail.com>
2015-07-17 22:04:05 -0700

    new form for reminders

--- reminders/forms.py
+++ reminders/forms.py
@@ -1,6 +1,8 @@
-#from reminders.models import UserProfile
+from reminders.models import Reminder
 from django.contrib.auth.models import User
 from django import forms
+
+EMPTY_REMINDER_TITLE_ERROR = 'Reminders need titles!'
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
@@ -13,3 +15,18 @@ class UserProfileForm(forms.ModelForm):
 	class Meta:
 		#model = UserProfile
 		fields = ('phone',)
+
+class ReminderForm(forms.ModelForm):
+	
+	class Meta:
+		model = Reminder
+		fields = ('title',)
+		widgets = {
+			'title': forms.fields.TextInput(attrs={
+				'placeholder': 'Enter a reminder',
+			}),
+		}
+		error_messages = {
+			'title': {'required': EMPTY_REMINDER_TITLE_ERROR}
+		}
+	
--- reminders/tests/test_forms.py
+++ reminders/tests/test_forms.py
@@ -1,1 +1,17 @@
 from django.test import TestCase
+
+from reminders.forms import ReminderForm, EMPTY_REMINDER_TITLE_ERROR
+
+class ReminderFormTest(TestCase):
+
+	def test_form_renders_reminder_text_input(self):
+		form = ReminderForm()
+		self.assertIn('placeholder="Enter a reminder"', form.as_p())
+
+	def test_form_validation_for_blank_reminders(self):
+		form = ReminderForm(data={'text': ''})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(
+			form.errors['title'],
+			[EMPTY_REMINDER_TITLE_ERROR]
+		)
