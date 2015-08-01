0faf146eedc2c9d0d05a17f0254c76752a8a2ce0
Jim Triphahn <jimmtriphahn@gmail.com>
2015-08-01 14:37:18 -0700

    User creation and registration done.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import resolve
 from accounts.views import account_login, account_register
 from accounts.forms import LoginForm, RegisterForm
 from accounts.models import CustomUser
+from .base import VALID_USER
 
 class AccountsLoginPageTest(TestCase):
 
@@ -48,7 +49,8 @@ class AccountsLoginPageTest(TestCase):
 		pass
 
 class AccountsRegisterPageTest(TestCase):
-	
+	maxDiff = None
+
 	def test_register_url_resolves_to_account_register_view(self):
 		found = resolve('/accounts/register/')
 		self.assertEqual(found.func, account_register)
@@ -62,12 +64,57 @@ class AccountsRegisterPageTest(TestCase):
 		request = HttpRequest()
 		response = account_register(request)
 
-		expected_html = render_to_string('accounts/register.html')
+		expected_html = render_to_string('accounts/register.html', {'form': RegisterForm()})
 
-		self.assertEqual(response.content.decode(), expected_html)
+		self.assertMultiLineEqual(response.content.decode(), expected_html)
+
+	def test_register_page_only_does_not_save(self):
+		request = HttpRequest()
+		account_register(request)
+		self.assertEqual(CustomUser.objects.count(), 0)
 
 	def test_register_form_is_used(self):
 		response = self.client.get(
 			'/accounts/register/'
 		)
 		self.assertIsInstance(response.context['form'], RegisterForm)
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post(
+			'/accounts/register/',
+			data=VALID_USER
+		)
+		self.assertEqual(CustomUser.objects.count(), 1)
+		saved_user = CustomUser.objects.first()
+		self.assertEqual(saved_user.email, 'jj@gmail.com')
+		self.assertNotEqual(saved_user.password, '123')
+
+	def test_POST_request_redirects_to_login(self):
+		response = self.client.post(
+			'/accounts/register/',
+			data=VALID_USER
+		)
+		self.assertRedirects(response, '/accounts/login/')
+
+	def test_invalid_POST_renders_register_form(self):
+		response = self.client.post(
+			'/accounts/register/',
+			data={
+				'email':'',
+				'password1': '',
+				'password2': ''
+			}
+		)
+		self.assertIsInstance(response.context['form'], RegisterForm)
+
+	def test_invalid_POST_displays_errors(self):
+		response = self.client.post(
+			'/accounts/register/',
+			data={
+				'email':'',
+				'password1': '',
+				'password2': ''
+			}
+		)
+		self.assertIn('An email address is required', response.content.decode())
+		self.assertIn('Password cannot be blank', response.content.decode())
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from accounts.forms import LoginForm, RegisterForm
 
@@ -8,4 +8,10 @@ def account_login(request):
 	return render(request, 'accounts/login.html', {'form': LoginForm()})
 
 def account_register(request):
-	return render(request, 'accounts/register.html', {'form': RegisterForm()})
+	form = RegisterForm()
+	if request.method == 'POST':
+		form = RegisterForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('account_login')
+	return render(request, 'accounts/register.html', {'form': form})
