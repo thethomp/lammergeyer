946c39c052e0b6ee80a35a8c302b2725a23eb2ff
Jim Triphahn <jimmtriphahn@gmail.com>
2015-08-05 19:37:11 -0700

    removed unused code

--- reminders/urls.py
+++ reminders/urls.py
@@ -3,14 +3,6 @@ from django.conf.urls import patterns, url
 from reminders import views
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
-	url(r'^register/$', views.register, name='register'),
-	url(r'^login/$', views.user_login, name='login'),
-	url(r'^logged-in/$', views.user_logged_in, name='logged-in'),
-	#url(r'^home/$', views.home, name='home'),
-	url(r'^account/$', views.account, name='account'),
-	url(r'^about/$', views.about, name='about'),
-	url(r'^logout/$', 'django.contrib.auth.views.logout', name="logout"),
 	url(r'^new$', views.new_reminder_list, name='new_reminder_list'),
 	url(r'^(\d+)/$', views.view_reminders, name='view_reminders'),
 	url(r'^(?P<list_id>\d+)/edit_reminder/(?P<pk>\d+)$', views.edit_reminder, name='edit_reminder'),
